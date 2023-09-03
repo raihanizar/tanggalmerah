334d21dd6181c10a3ddef572b4959dce1f070a62
Raihan Nizar <raihannizar24@gmail.com>
2023-09-03 22:51:59 +0700

    move img to public to override auto-generated og links

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,14 +5,15 @@ import { Analytics } from '@vercel/analytics/react'
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const metadata = {
+  metadataBase: new URL(`${baseUrl}`),
   title: 'Cek Tanggal Merah',
   description: 'Kapan tanggal merah terdekat? Cek di sini!',
   keywords: ['liburan', 'holiday', 'tanggal', 'merah', 'kalender', 'indonesia'],
   openGraph: {
     title: 'Cek Tanggal Merah',
     description: 'Kapan tanggal merah terdekat? Cek di sini!',
-    images: [`${baseUrl}/opengraph-image.png`],
-    url: baseUrl,
+    images: '/og.png',
+    url: '/',
     siteName: 'Cek Tanggal Merah',
     locale: 'id_ID',
     type: 'website',
@@ -21,7 +22,7 @@ export const metadata = {
     card: 'summary',
     title: 'Cek Tanggal Merah',
     description: 'Kapan tanggal merah terdekat? Cek di sini!',
-    images: [`${baseUrl}/twitter-image.png`]
+    images: '/og.png'
   },
 }
 
