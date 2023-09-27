df6db175f4df121247bcc5f7213be97ca410f2be
Raihan Nizar <raihannizar24@gmail.com>
2023-09-28 01:02:19 +0700

    fix bug for condition today is holiday

--- src/app/components/InfoTglMerah.jsx
+++ src/app/components/InfoTglMerah.jsx
@@ -7,22 +7,51 @@ async function getMerah(todayDate, dateOptions) {
       },
       cache: "no-store" // SSR in app router
     })
+    const resultJson = await res.json()
 
-    const merahJson = await res.json()
+    // only national holidays will be considered
+    let merahJson = resultJson.filter(result => result.is_national_holiday === true);
+
+    let merahString = ''
     let merahIDString = ''
     let merahName = ''
     let nearestTimeToMerah = ''
+    let nextMerahString = ''
+    let nextMerahIDString = ''
+    let nextMerahName = ''
 
     for (let i = 0; i < merahJson.length; i++) {
-      if (merahJson[i].is_national_holiday) {
-        let merahString = merahJson[i].holiday_date
-        let merahDate = new Date(merahString)
-        // because holiday_date on json response is descending...
-        let timeToMerah = merahDate - todayDate
-        if (timeToMerah > 0) {
-          merahIDString = merahDate.toLocaleDateString('id-ID', dateOptions)
-          merahName = merahJson[i].holiday_name
-          nearestTimeToMerah = timeToMerah
+      merahString = merahJson[i].holiday_date
+
+      // if stops on first element, next holiday must be new year
+      if (i === 0) {
+        let merahStringParts = merahString.split("-")
+        let merahYear = parseInt(merahStringParts[0])
+        nextMerahString = `${merahYear + 1}-01-01`
+      } else {
+        nextMerahString = merahJson[i - 1].holiday_date
+      }
+
+      let merahDate = dateStringToDate(merahString)
+      let nextMerahDate = dateStringToDate(nextMerahString)
+
+      let timeToMerah = merahDate - todayDate
+
+      // loop merahDate descending order, get date difference, until merahDate preceeds todayDate by 1 day
+      if (timeToMerah >= -86400000) {
+        merahIDString = merahDate.toLocaleDateString('id-ID', dateOptions)
+        merahName = merahJson[i].holiday_name
+        nearestTimeToMerah = timeToMerah
+
+        // next holiday information
+        nextMerahIDString = nextMerahDate.toLocaleDateString('id-ID', dateOptions)
+        // if stops on first element, next holiday must be new year
+        if (i === 0) {
+          let merahStringParts = merahString.split("-")
+          let merahYear = parseInt(merahStringParts[0])
+          nextMerahName = `Tahun baru ${merahYear + 1}`
+        } else {
+          nextMerahName = merahJson[i - 1].holiday_name
         }
       }
     }
@@ -33,7 +62,9 @@ async function getMerah(todayDate, dateOptions) {
     let nearestHoliday = {
       holiday_date: merahIDString,
       holiday_name: merahName,
-      days_to_holiday: nearestDaysToMerah
+      days_to_holiday: nearestDaysToMerah,
+      next_holiday_date: nextMerahIDString,
+      next_holiday_name: nextMerahName
     }
     return nearestHoliday
 
@@ -42,22 +73,47 @@ async function getMerah(todayDate, dateOptions) {
   }
 }
 
+function dateStringToDate(dateString) {
+  let dateStringParts = dateString.split("-")
+  var year = parseInt(dateStringParts[0])
+  var month = parseInt(dateStringParts[1]) - 1
+  var day = parseInt(dateStringParts[2])
+  return new Date(year, month, day)
+}
+
+function openingMsg(days) {
+  if (days > 0) {
+    return 'Jadi, kapan tanggal merah terdekat?'
+  } else {
+    return 'Hari ini tanggal merah! 🎉'
+  }
+}
+
 function untilMerahMsg(days) {
   if (days > 5) {
     return `Sabar, masih ${days} hari lagi...`
   } else if (days > 1) {
     return `Siap-siap, liburan tinggal ${days} hari lagi!`
+  } else if (days === 0) {
+    return `Selamat liburan... 🏖️`
   } else {
     return "Siap-siap, besok libur!"
   }
 }
 
-function merahGapMsg(merahString) {
-  const weekdayString = merahString.split(", ")[0]
-  if (weekdayString === "Selasa" || weekdayString === "Kamis") {
-    return "Tanggal merah dekat hari kejepit tuh, bisa siapkan cuti. 👌"
-  } else if (weekdayString === "Senin" || weekdayString === "Jumat") {
-    return "Tanggal merah dan weekend berturut-turut tuh, siap-siap liburan panjang! 🏖️"
+function bottomMsg(merah) {
+  // if today is tanggal merah, show next holiday info
+  if (merah.days_to_holiday === 0) {
+    return `Selanjutnya: ${merah.next_holiday_date} (${merah.next_holiday_name})`
+
+    // else, show off-work info & strategy
+  } else {
+    let weekdayString = merah.holiday_date.split(", ")[0]
+    if (weekdayString === "Selasa" || weekdayString === "Kamis") {
+      return "Tanggal merah dekat hari kejepit tuh, bisa siapkan cuti. 👌"
+    } else if (weekdayString === "Senin" || weekdayString === "Jumat") {
+      return "Tanggal merah dan weekend berturut-turut tuh, siap-siap liburan panjang! 🏖️"
+    }
   }
 }
 
@@ -68,7 +124,7 @@ async function InfoTglMerah({ todaysDate, dateOptions }) {
   return (
     <div className="flex flex-col justify-between items-center">
       <div className="flex flex-col justify-between items-center">
-        <h1 className="text-center text-md md:text-lg lg:text-xl mb-2 md:mb-3 lg:mb-4 font-bold text-black">Jadi, kapan tanggal merah terdekat?</h1>
+        <h1 className="text-center text-md md:text-lg lg:text-xl mb-2 md:mb-3 lg:mb-4 font-bold text-black">{openingMsg(merah.days_to_holiday)}</h1>
         <p className="text-center text-3xl md:text-4xl lg:text-5xl font-bold text-orange">{merah.holiday_date}</p>
         <p className="text-center text-xl md:text-2xl lg:text-3xl font-bold text-orange">{merah.holiday_name}</p>
       </div>
@@ -76,7 +132,7 @@ async function InfoTglMerah({ todaysDate, dateOptions }) {
         <p className="text-center text-md md:text-lg lg:text-xl font-bold text-black">{untilMerahMsg(merah.days_to_holiday)}</p>
       </div>
       <div className="flex flex-col justify-between items-center mt-6 md:mt-8 lg:mt-10 p-1 md:p-2 bg-shadeivory rounded-lg">
-        <p className="text-center text-xs font-bold text-darkivory">{merahGapMsg(merah.holiday_date)}</p>
+        <p className="text-center text-xs font-bold text-darkivory">{bottomMsg(merah)}</p>
       </div>
     </div>
   )
